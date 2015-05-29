3a369dd41137657fa8611b1ec685afe7a8af910e
Nilambar Sharma <nilambar@outlook.com>
2015-05-29 14:40:39 +0545

    add header and footer file

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-
-  <header>
-    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-    <h3 class="site-description"><?php bloginfo( 'description' ); ?></h3>
-  </header>
+<?php get_header();?>
 
   <?php if ( have_posts() ) : ?>
 
@@ -51,6 +37,4 @@
   }
 ?>
 
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer();?>
