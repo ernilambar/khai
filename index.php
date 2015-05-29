d944c5a73bfe5c13cf7acd79fc5df4dcca82b073
Nilambar Sharma <nilambar@outlook.com>
2015-05-29 14:22:11 +0545

    add index file

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php wp_head(); ?>
+</head>
+<body>
+  <?php if ( have_posts() ) : ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php
+        get_template_part( 'template-parts/content', get_post_format() );
+      ?>
+
+    <?php endwhile; ?>
+
+    <?php the_posts_navigation(); ?>
+
+  <?php else : ?>
+
+    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+  <?php endif; ?>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
